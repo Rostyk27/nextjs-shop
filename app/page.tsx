58b0d04f42addc708f7e9fd157333e84826e7432
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-05-11 14:28:58 +0300

    using external data to fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import Product from '@/types/Product';
-import path from 'path';
-import { promises as fs } from 'fs';
 
 const getData = async () => {
   const res = await fetch('https://reactshop.rostyk.dev/products-data.json');
-  // const res = await fetch('data.json');
   const jsonFile: { products: Product[] } = await res.json();
   const products: Product[] = jsonFile.products;
 
@@ -12,7 +9,6 @@ const getData = async () => {
     throw new Error('Failed to fetch data');
   }
 
-  console.log(products);
   return products;
 };
 
@@ -25,7 +21,7 @@ export default async function Home() {
         <h1>All products</h1>
 
         <ul className="product__list grid grid-cols-1 gap-x-6 gap-y-14  sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products?.map(product => (
+          {products?.map((product: Product) => (
             <li key={product.id}>
               <h3>{product.name}</h3>
               <img src={product.imageSrc} />
